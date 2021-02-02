b8a4c3809ccca7ad6b3756c344333aeb7297b599
EmilyLow <emlow@me.com>
2021-02-02 14:59:12 -0600

    Basic navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import origamiBird from "./images/origami_bird_icon.svg";
+
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+     <Navbar bg="light" expand="lg">
+     <img
+        src={origamiBird}
+        width="50"
+        height="50"
+      />
+       The Nest
+       </Navbar>
+    </Container>
   );
 }
 
---
+++ src/sampleData.js
@@ -0,0 +1,36 @@
+import birdHouse from './images/bird_house.jpeg';
+import blueBird from "./images/blue_bird.jpeg";
+import migration from "./images/migration.jpeg";
+import pigeons from "./images/pigeons.jpeg";
+
+let sampleData = [
+    {
+        title: "Pigeons: Nuisance or Necessity?",
+        blurb: "These so called rats of the city might be more important than we think, scientific study claims.",
+        author: "Andrea Woodcutter",
+        imgSrc: pigeons
+    }, 
+    {
+        title: "Return of the Bluebells",
+        blurb: "After decades on the edge of extinction, these birds are making a comeback.",
+        author: "Timothy Strange",
+        imgSrc: blueBird
+    }, 
+    {
+        title: "Bird Hostels",
+        blurb: "Elementary schools all over the western Europe are creating hideaways for our migratory friends.",
+        author: "Nellie Wilson",
+        imgSrc: birdHouse
+    }, 
+    {
+        title: "A Busy Spring",
+        blurb: "A migration of previously unrecorded scale is expected this spring.",
+        author: "Austen Farrel",
+        imgSrc: migration
+    }, 
+
+
+];
+
+
+export default sampleData;
